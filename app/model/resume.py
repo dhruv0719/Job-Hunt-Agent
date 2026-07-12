45566a187129ce130229a9b2e0c33f3391f5ed5d
Dhruv Patil <patildhruv46@gmail.com>
2026-07-12 14:32:22 +0530

    Dataclass of resume

---
+++ app/model/resume.py
@@ -0,0 +1,42 @@
+# app/model/resume.py
+
+from typing import List, Dict
+from dataclasses import dataclass, field
+
+
+@dataclass
+class Experience:
+    company: str
+    role: str
+    duration: str
+    description: str
+
+@dataclass
+class Education:
+    institution: str
+    degree: str
+    field_of_study: str
+    start_year: str
+    end_year: str
+
+@dataclass
+class Project:
+    title: str
+    description: str
+    technologies: List[str]
+    link: str
+
+
+@dataclass
+class Resume:
+    name: str
+    email: str
+    phone_number: str
+    skills: List[str] = field(default_factory=list)
+    experience: List[Experience]
+    education: List[Education]
+    certifications: List[str]
+    projects: List[Project]
+    languages: List[str] = field(default_factory=list)
+    links: List[str]
+    miscellaneous: Dict[str, str]
